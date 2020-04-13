1c85e5dddfd4ab69d6e5e676d179cdc1c6792060
Fenil Lad <fenil23lad@gmail.com>
2020-04-13 14:33:27 -0400

    With Node module

---
+++ Capstone_final_FrontEnd/src/app/login/login.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../service/user.service";
+import {NotificationService} from "../service/notification.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+      users: any = {};
+
+    constructor(private userService: UserService,
+                private notify: NotificationService,
+                private router: Router) {
+    }
+
+  ngOnInit() {
+  
+  }
+  
+  onLogin(){
+      console.log('users', this.users);
+        this.userService.userLogin(this.users).subscribe((res) => {
+                console.log('res', res);
+                if (res) {
+                    this.notify.showSuccess('Login Successfully..!');
+                    localStorage.setItem('email', res[0].email);
+                    this.router.navigate(['/home']);
+                }
+            }, error => {
+                this.notify.showError(error.message);
+                console.log('error', error);
+            });
+    }
+
+    onSignUp() {
+        this.router.navigate(['/register']);
+    }
+
+}
